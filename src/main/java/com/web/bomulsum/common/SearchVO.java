5438118d5fb279b1eae3b104d246754bc84eeb7a
DESKTOP-95BTHUM\dndud <dndud1997@naver.com>
2020-09-28 13:48:28 +0900

    ㅇㅇㅇ

--- src/main/java/com/web/bomulsum/common/SearchVO.java
+++ src/main/java/com/web/bomulsum/common/SearchVO.java
@@ -1,5 +1,25 @@
 package com.web.bomulsum.common;
 
-public class SearchVO {
+public class SearchVO extends PageVO {
 
+	private String keyword;
+	private String condition;
+	
+	public SearchVO() {
+		this.keyword = "";
+		this.condition = "";
+	}
+	
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
 }
